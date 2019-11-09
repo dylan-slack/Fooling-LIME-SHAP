9c0e42771b4b6b75b7ba76b9b7ad01f9ef6c0b87
gramsbyDill <dylanzslack@gmail.com>
2019-11-09 15:20:45 -0800

    update name

--- utils.py
+++ utils.py
@@ -35,7 +35,7 @@ class Params():
 
 
 def one_hot_encode(y):
-    """ One hot encode y.  We use this to get from 1 dim ys to predict proba's.
+    """ One hot encode y for binary features.  We use this to get from 1 dim ys to predict proba's.
     This is taken from this s.o. post: https://stackoverflow.com/questions/29831489/convert-array-of-indices-to-1-hot-encoded-numpy-array
 
     Parameters
@@ -49,3 +49,70 @@ def one_hot_encode(y):
     y_hat_one_hot = np.zeros((len(y), 2))
     y_hat_one_hot[np.arange(len(y)), y] = 1
     return y_hat_one_hot
+
+def rank_features(explanation):
+    """ Given an explanation of type (name, value) provide the ranked list of feature names according to importance
+
+    Parameters
+    ----------
+    explanation : list
+
+    Returns
+    ----------
+    List contained ranked feature names
+    """
+    ordered_tuples = sorted(explanation, key=lambda x : abs(x[1]), reverse=True)   
+    return [tup[0] for tup in ordered_tuples]
+
+def get_rank_map(ranks, to_consider):
+    """ Give a list of feature names in their ranked positions, return a map from position ranks
+    to pct occurances.
+
+    Parameters
+    ----------
+    ranks : list
+    to_consider : int
+
+    Returns
+    ----------
+    A dictionary containing the ranks mapped to the uniques.
+    """
+    unique = {i+1 : [] for i in range(len(ranks))}
+
+    for i, rank in enumerate(ranks):
+        for unique_rank in np.unique(rank):
+            unique[i+1].append((unique_rank, np.sum(np.array(rank) == unique_rank) / to_consider))
+
+    return unique
+
+def experiment_summary(explanations, features):
+    """ Provide a high level display of the experiment results for the top three features.
+    This should be read as the rank (e.g. 1 means most important) and the pct occurances
+    of the features of interest.
+
+    Parameters
+    ----------
+    explanations : list
+    explain_features : list
+    bias_feature : string
+
+    Returns 
+    ----------
+    A summary of the experiment
+    """
+    # features_of_interest = explain_features + [bias_feature]   
+    top_features = [[], [], []]
+
+    # sort ranks into top 3 features
+    for exp in explanations:
+        ranks = rank_features(exp)
+        for i in range(3):
+            for f in features:
+                if f in ranks[i]:
+                    top_features[i].append(f)
+
+    return get_rank_map(top_features, len(explanations))
+
+
+
+
